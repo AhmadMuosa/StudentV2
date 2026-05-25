14cdc0814374370f489a66c3f362af615f622cec
AhmadMuosa <muosa@sheridancollege.ca>
2026-05-25 08:55:07 -0400

    test version StudentTest class added

--- src/testprj/StudentTest.java
+++ src/testprj/StudentTest.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package testprj;
 
 /**
  *
@@ -23,5 +24,4 @@ public class StudentTest {
     public void setName(String name) {
         this.name = name;
     }
-    
 }
