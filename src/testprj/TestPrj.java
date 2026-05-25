a9c4aa0ff9320cf46bae9819e998bbf37a785be2
AhmadMuosa <muosa@sheridancollege.ca>
2026-05-25 09:59:24 -0400

    Update TestPrj.java

--- src/testprj/TestPrj.java
+++ src/testprj/TestPrj.java
@@ -15,7 +15,7 @@ public class TestPrj {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Salam");
+        System.out.println("Hi");
     }
     
 }
